2f835ebe745f94bd8bcdc8c7129ce1fbd47cf31b
BeishebaevEldar <148213957+BeishebaevEldar@users.noreply.github.com>
2024-03-20 09:50:34 +0600

    Add files via upload

---
+++ c1.js
@@ -0,0 +1,84 @@
+{
+  let heart = document.getElementById("heart");
+  heart.onclick = () => {
+    if (heart.classList == "fa-regular fa-heart") {
+      heart.classList = "fa-solid fa-heart";
+    } else if (heart.classList == "fa-solid fa-heart") {
+      heart.classList = "fa-regular fa-heart";
+    }
+  };
+}
+
+{
+  let heart = document.getElementById("h1");
+  heart.onclick = () => {
+    if (heart.classList == "fa-regular fa-heart") {
+      heart.classList = "fa-solid fa-heart";
+    } else if (heart.classList == "fa-solid fa-heart") {
+      heart.classList = "fa-regular fa-heart";
+    }
+  };
+}
+
+{
+  let heart = document.getElementById("h2");
+  heart.onclick = () => {
+    if (heart.classList == "fa-regular fa-heart") {
+      heart.classList = "fa-solid fa-heart";
+    } else if (heart.classList == "fa-solid fa-heart") {
+      heart.classList = "fa-regular fa-heart";
+    }
+  };
+}
+{
+  let heart = document.getElementById("h3");
+  heart.onclick = () => {
+    if (heart.classList == "fa-regular fa-heart") {
+      heart.classList = "fa-solid fa-heart";
+    } else if (heart.classList == "fa-solid fa-heart") {
+      heart.classList = "fa-regular fa-heart";
+    }
+  };
+}
+{
+  let heart = document.getElementById("h4");
+  heart.onclick = () => {
+    if (heart.classList == "fa-regular fa-heart") {
+      heart.classList = "fa-solid fa-heart";
+    } else if (heart.classList == "fa-solid fa-heart") {
+      heart.classList = "fa-regular fa-heart";
+    }
+  };
+}
+{
+  let heart = document.getElementById("h5");
+  heart.onclick = () => {
+    if (heart.classList == "fa-regular fa-heart") {
+      heart.classList = "fa-solid fa-heart";
+    } else if (heart.classList == "fa-solid fa-heart") {
+      heart.classList = "fa-regular fa-heart";
+    }
+  };
+}
+
+{
+  let heart = document.getElementById("h6");
+  heart.onclick = () => {
+    if (heart.classList == "fa-regular fa-heart") {
+      heart.classList = "fa-solid fa-heart";
+    } else if (heart.classList == "fa-solid fa-heart") {
+      heart.classList = "fa-regular fa-heart";
+    }
+  };
+}
+{
+  let heart = document.getElementById("h7");
+  heart.onclick = () => {
+    if (heart.classList == "fa-regular fa-heart") {
+      heart.classList = "fa-solid fa-heart";
+    } else if (heart.classList == "fa-solid fa-heart") {
+      heart.classList = "fa-regular fa-heart";
+    }
+  };
+}
+
